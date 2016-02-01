c0c1059d5b63cfe604218a3616d994834899a3e0
Gabriele Petronella <gabriele@buildo.io>
2016-02-01 16:10:03 +0100

    Minor tweaks

--- src/app/routes.js
+++ src/app/routes.js
@@ -3,10 +3,6 @@ import { Route } from 'react-router';
 import App from 'AppContainer';
 import Hello from 'HelloContainer';
 
-// FIXME(gabro)
-// revenge currently looks for the `handler`
-// prop, no longer there in react-router 1.0+
-// That's why `handler` is there.
 export default (
   <Route component={App} handler={App}>
     <Route path='/' component={Hello} handler={Hello} />
