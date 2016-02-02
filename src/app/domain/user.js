1bd18aabb08ec94d5a1cb04a57f4fc8208e7e1eb
Gabriele Petronella <gabriele@buildo.io>
2016-02-02 15:00:03 +0100

    Improve User domain

--- src/app/domain/user.js
+++ src/app/domain/user.js
@@ -1,5 +1,10 @@
 import { t } from 'revenge';
 
+const Gender = t.enums.of(['male', 'female'], 'Gender');
+
 export default t.struct({
-  name: t.String
+  name: t.String,
+  surname: t.String,
+  gender: Gender,
+  region: t.String
 });
