22985de720cd895592ea75a8f28fe7938f275bd2
Gabriele Petronella <gabriele@buildo.io>
2016-02-02 12:09:56 +0100

    Clearer grouping of exports in Basic/index.js

--- src/app/components/Basic/index.js
+++ src/app/components/Basic/index.js
@@ -1,9 +1,13 @@
+// buildo-react-components cherry-picks
+export { FlexView } from 'buildo-react-components/src/flex';
+export Button from 'buildo-react-components/src/button';
+export Icon from 'buildo-react-components/src/Icon';
+
+// buildo-react-components customizations
+export LoadingSpinner from './LoadingSpinner/LoadingSpinner';
+
+// local definitions
 export {
   intl, FormattedDate, FormattedNumber, FormattedRelative, FormattedTime, FormattedMessage, intlMethods, translateProps,
   childContextTypes, getChildContext, contextTypes
 } from './intlHelpers/';
-
-export { FlexView } from 'buildo-react-components/src/flex';
-export Button from 'buildo-react-components/src/button';
-export Icon from 'buildo-react-components/src/Icon';
-export LoadingSpinner from './LoadingSpinner/LoadingSpinner';
